6cc9b90fa048213a5532d3f213f7b7aeaa7572cf
Vina Melody <vina_melody@yahoo.com>
2020-11-29 22:57:41 +0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Vina Melody on 29/11/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
